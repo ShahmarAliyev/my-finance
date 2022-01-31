18461b1a7b83bc7e6d21dda1a76119785afd931d
ShahmarALiyev <saliyev2019@gmail.com>
2022-01-31 16:42:46 -0600

    Creating a transaction form

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,15 @@
 // styles
 import styles from "./home.module.css";
+//components
+import TransactionForm from "./TransactionForm";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
