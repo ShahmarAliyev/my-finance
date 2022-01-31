fd1404268c5777d1ed7e13740d37952647fb143e
ShahmarALiyev <saliyev2019@gmail.com>
2022-01-31 12:18:07 -0600

    setting up firebase and firebase auth

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "forebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAo51CSeI9_zLSD4XpJVEsVAcGMQG6CdJs",
+  authDomain: "rocket-finance.firebaseapp.com",
+  projectId: "rocket-finance",
+  storageBucket: "rocket-finance.appspot.com",
+  messagingSenderId: "263774040602",
+  appId: "1:263774040602:web:156468d80fe041c3ae57a3",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+export { projectFirestore, projectAuth };
