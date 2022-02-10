44177e25fd2951bc0c285904eac97c0588aa72aa
ShahmarALiyev <saliyev2019@gmail.com>
2022-02-10 17:43:44 -0600

    deployment fix

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -12,20 +12,23 @@ export default function Login() {
     e.preventDefault();
     login(email, password);
   };
-  const handleChangePassword = (e) => setPassword(e.traget.value);
-  const handleEmailChange = (e) => setEmail(e.traget.value);
+
   return (
     <form onSubmit={handleSubmit} className={styles["login-form"]}>
       <h2>login</h2>
       <label>
         <span>email:</span>
-        <input type="email" onChange={handleEmailChange} value={email} />
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
       </label>
       <label>
         <span>password:</span>
         <input
           type="password"
-          onChange={handleChangePassword}
+          onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
       </label>
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,10 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
         test: /\.?js$/,
+        loader: "source-map-loader",
+
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
